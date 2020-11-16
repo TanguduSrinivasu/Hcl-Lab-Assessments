50df830f355ee783144c6da5c587ace4506c8706
TanguduSrinivasu <46299341+TanguduSrinivasu@users.noreply.github.com>
2020-11-16 18:43:53 +0530

    Add files via upload

---
+++ Day2/src/com/pojo/A.java
@@ -0,0 +1,18 @@
+package com.pojo;
+
+public class A {
+	private String name = "Srinu";
+
+	public void display() {
+		System.out.println("Welcome to Java");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
---
+++ Day2/src/com/pojo/B.java
@@ -0,0 +1,13 @@
+package com.pojo;
+
+public class B extends A {
+	private int rollNo = 1256;
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+}
